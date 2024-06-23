999315c5fbd118855839a9b3757178404463e781
Cole Flannery <jaidencoleflannery@gmail.com>
2024-06-23 16:48:47 -0500

    message functionality when no tasks are found

--- obj/Debug/net8.0/TodoApi.AssemblyInfo.cs
+++ obj/Debug/net8.0/TodoApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5ba1df8196020cd02bb9476f91de7909988d8fbc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+84dbb08548a25edd025e9d70dd254aeffe2cc465")]
 [assembly: System.Reflection.AssemblyProductAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
